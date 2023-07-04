6383cb55a1b7258a057f9a47424cc5c33bf89722
estebanrm1 <solsel90@gmail.com>
2023-07-04 12:44:59 -0300

    Terminamos de agregar los middleware a la configuracion inicial del proyecto

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 //usar puerto
 const app = express();
@@ -12,6 +14,10 @@ app.listen(app.get('port'), ()=>{
 
 //middlewares: funciones que se ejecutan antes de las rutas
 app.use(cors()); //permitir conexiones remotas
+app.use(express.json()); // permite a mi app recibir objetos de tipo json en los request
+app.use(morgan('dev')); // Muestra en consola info extra de las solicitudes
+console.log(path.join(__dirname, '/public')); //La variable __dirname siempre devuelve la ruta absoluta de donde vive el proyecto 
+app.use(express.static(path.join(__dirname, '/public'))); //Esto nos permite poder ejecutar los archivos estaticos de mi proyecto en la ruta raiz de mi backend ej: http://localhost:4000
 
 
 //rutas
